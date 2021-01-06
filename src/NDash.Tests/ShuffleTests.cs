bf88ec7be02924cc6bb28d27ef39d4f425482633
Joseph Hart <josephrhart@gmail.com>
2021-01-05 19:54:20 -0600

    feat: adding Shuffle extension

---
+++ src/NDash.Tests/ShuffleTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NDash.Tests
+{
+    public class ShuffleTests
+    {
+        class ReverseRandom : Random
+        {
+            readonly int[] order = new[] { 4, 3, 2, 3, 4 };
+            int curr = 0;
+
+            public override int Next(int maxValue) => order[curr++];
+        }
+
+        public class given_custom_RNG
+        {
+            [Fact]
+            public void should_shuffle_with_RNG()
+            {
+                var ascending = new[] { 1, 2, 3, 4, 5 };
+                var descending = ascending.Shuffle(new ReverseRandom());
+
+                Assert.Equal(new[] { 5, 4, 3, 2, 1 }, descending);
+            }
+        }
+
+        public class given_default_RNG
+        {
+            [Fact]
+            public void should_shuffle_randomly()
+            {
+                var ascending = Enumerable.Range(0, 100);
+
+                var shuffled = ascending.Shuffle();
+
+                // if this somehow fails, it's an act of god
+                Assert.NotEqual(shuffled, ascending);
+            }
+
+            [Fact]
+            public void should_not_mutate_original_collection()
+            {
+                var ascending = Enumerable.Range(0, 100);
+                
+                var shuffled = ascending.Shuffle();
+
+                Assert.Equal(Enumerable.Range(0, 100), ascending);
+            }
+        }
+    }
+}
---
+++ src/NDash/Shuffle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NDash
+{
+    public static partial class NDashLib
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
+        {
+            return collection.Shuffle(new Random());
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection, int seed)
+        {
+            return collection.Shuffle(new Random(seed));
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection, Random rng)
+        {
+            int count = collection.Count();
+
+            var shuffled = collection.ToList();
+
+            for (int i = 0; i < count; i++)
+            {
+                int next = rng.Next(count);
+
+                shuffled.Swap(i, next);
+            }
+
+            return shuffled;
+        }
+    }
+}

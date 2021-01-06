707cdedbcb3c271b73754af17999d24e8b7a7c20
Joseph Hart <josephrhart@gmail.com>
2021-01-05 20:52:32 -0600

    refactor: replacing test classes with record syntax

--- src/NDash.Tests/TestClasses/Car.cs
+++ src/NDash.Tests/TestClasses/Car.cs
@@ -1,20 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NDash.Tests.TestClasses
+﻿namespace NDash.Tests.TestClasses
 {
-    class Car
-    {
-        public string Make { get; private set; }
-        public string Model { get; private set; }
-        public int Year { get; private set; }
-
-        public Car(string make, string model, int year)
-        {
-            Make = make;
-            Model = model;
-            Year = year;
-        }
-    }
+    public record Car(string Make, string Model, int Year);
 }
--- src/NDash.Tests/TestClasses/Developer.cs
+++ src/NDash.Tests/TestClasses/Developer.cs
@@ -1,21 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NDash.Tests.TestClasses
+﻿namespace NDash.Tests.TestClasses
 {
-    class Developer
-    {
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
-
-        public Developer(string firstName, string lastName, int age)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Age = age;
-        }
-    }
-
+    public record Developer(string FirstName, string LastName, int Age);
 }

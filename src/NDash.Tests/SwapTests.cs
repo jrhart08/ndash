a2b8e91e7c4e666bc2adfc2c03025a5a7ed669fd
Joseph Hart <josephrhart@gmail.com>
2021-01-05 19:20:48 -0600

    feat: adding Swap (IList) extension

---
+++ src/NDash.Tests/SwapTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace NDash.Tests
+{
+    public class SwapTests
+    {
+        public class given_different_indices
+        {
+            [Fact]
+            public void should_swap_at_indices()
+            {
+                var hello = new[] { 'h', 'e', 'l', 'l', 'o' };
+
+                hello.Swap(1, 2);
+
+                Assert.Equal("hlelo", string.Join("", hello));
+            }
+        }
+
+        public class given_duplicate_index
+        {
+            [Fact]
+            public void should_remain_unchanged()
+            {
+                var goodbye = new[] { 'g', 'o', 'o', 'd', 'b', 'y', 'e' };
+
+                goodbye.Swap(0, 0);
+
+                Assert.Equal("goodbye", string.Join("", goodbye));
+            }
+        }
+    }
+}
---
+++ src/NDash/Swap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace NDash
+{
+    public static partial class NDashLib
+    {
+        /// <summary>
+        /// Swaps 2 elements in-place (READ: mutates the list)
+        /// </summary>
+        public static IList<T> Swap<T>(this IList<T> list, int i, int j)
+        {
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+
+            return list;
+        }
+    }
+}

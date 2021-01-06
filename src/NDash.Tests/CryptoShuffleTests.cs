bc457981d8bd20b6b342c7a39d1f097b68198982
Joseph Hart <josephrhart@gmail.com>
2021-01-06 00:42:53 -0600

    feat: adding CryptoShuffle just because

---
+++ src/NDash.Tests/CryptoShuffleTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NDash.Tests
+{
+    public class CryptoShuffleTests
+    {
+        [Fact]
+        public void should_not_mutate_original_collection()
+        {
+            var ascending = Enumerable.Range(0, 100);
+            ascending.CryptoShuffle();
+
+            var expected = Enumerable.Range(0, 100);
+            Assert.Equal(expected, ascending);
+            should_have_same_elements(expected, ascending);
+        }
+
+        [Fact]
+        public void should_shuffle_randomly()
+        {
+            var ascending = Enumerable.Range(0, 100);
+
+            var shuffled = ascending.CryptoShuffle();
+
+            // if this somehow fails, it's an act of god
+            Assert.NotEqual(ascending, shuffled);
+            should_have_same_elements(ascending, shuffled);
+        }
+
+        static void should_have_same_elements<T>(IEnumerable<T> expected, IEnumerable<T> actual)
+        {
+            Assert.Empty(expected.DisjunctiveUnion(actual));
+        }
+    }
+}
---
+++ src/NDash/CryptoShuffle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NDash
+{
+    public static partial class NDashLib
+    {
+        /// <summary>
+        /// Like Shuffle, but more secure, because reasons.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> CryptoShuffle<T>(this IEnumerable<T> collection)
+        {
+            var shuffled = collection.ToList();
+            int count = shuffled.Count;
+
+            for(int i = 0; i < count; i++)
+            {
+                int next = RandomNumberGenerator.GetInt32(count);
+
+                shuffled.Swap(i, next);
+            }
+
+            return shuffled;
+        }
+    }
+}
